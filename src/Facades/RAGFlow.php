d40c44d530744606e8d86848339885071806920b
s9 <lyhiving@qq.com>
2025-06-16 19:15:22 +0800

    BIG CHANGE REMOVE COMPONTS UNUSE

--- src/Facades/RAGFlow.php
+++ src/Facades/RAGFlow.php
@@ -11,20 +11,10 @@ use RAGFlow\Responses\StreamResponse;
 
 /**
  * @method static \RAGFlow\Resources\Assistants assistants()
- * @method static \RAGFlow\Resources\Audio audio()
- * @method static \RAGFlow\Resources\Batches batches()
  * @method static \RAGFlow\Resources\Chat chat()
  * @method static \RAGFlow\Resources\Completions completions()
- * @method static \RAGFlow\Resources\Embeddings embeddings()
- * @method static \RAGFlow\Resources\Edits edits()
  * @method static \RAGFlow\Resources\Files files()
  * @method static \RAGFlow\Resources\Chunks chunks()
- * @method static \RAGFlow\Resources\FineTunes fineTunes()
- * @method static \RAGFlow\Resources\Images images()
- * @method static \RAGFlow\Resources\Models models()
- * @method static \RAGFlow\Resources\Moderations moderations()
- * @method static \RAGFlow\Resources\Threads threads()
- * @method static \RAGFlow\Resources\VectorStores vectorStores()
  */
 final class RAGFlow extends Facade
 {
